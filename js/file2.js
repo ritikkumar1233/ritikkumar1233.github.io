7166db2cb176c64c0bb3e11f73c2d037affc08a1
ritikkumar1233 <ritikroushan96905@gmail.com>
2025-06-20 14:47:10 +0530

    update

--- js/file2.js
+++ js/file2.js
@@ -1,9 +1,36 @@
 //  operators
-let a = 10;
-let b = 20;
-console.log(a+b)
-console.log(b-a)
-console.log(a*b)
-console.log(b/a)
-console.log(b%a)
-console.log(2**4)
+// let a = 10;
+// let b = 20;
+// console.log(a+b)
+// console.log(b-a)
+// console.log(a*b)
+// console.log(b/a)
+// console.log(b%a)
+// console.log(2**4)
+
+// a = 15
+// a = a + 1;
+// console.log(a)
+// a++
+// console.log(a)
+// a+=3
+// console.log(a)
+
+// let a = 5
+// let b = 2
+// console.log(a>b)
+// console.log(!a>b)
+// console.log(a<b)
+
+
+// let a = "10"
+// let b = 10
+// console.log(a==b)
+// console.log(a===b)
+
+let a = 5
+let b = 3
+let c = 1
+console.log(a>b && a>c)
+console.log(a>b && a<c)
+console.log(a>b || a>c)
