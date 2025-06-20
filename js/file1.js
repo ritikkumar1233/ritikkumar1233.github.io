a6a642d885fbb5389ec14e239060cec1e5b9db0a
ritikkumar1233 <ritikroushan96905@gmail.com>
2025-06-20 14:34:12 +0530

    update

--- js/file1.js
+++ js/file1.js
@@ -1,4 +1,36 @@
-let a=10;
-let b=20;
-let c=a+b;
-console.log(c)
+// let a=10;
+// let b=20;
+// let c=a+b;
+// console.log(c)
+
+// let studentName = "Jhon"
+// let age = 20
+// let promoted = true
+// let score
+// let city = null
+// console.log(studentName+" => "+typeof studentName)
+// console.log(age+" => "+typeof age)
+// console.log(promoted+" => "+typeof promoted)
+// console.log(score+" => "+typeof score)
+// console.log(city=" => "+typeof city)
+
+// let age = "10"
+// console.log(typeof age)
+// let ageNum = Number(age)
+// console.log(typeof ageNum)
+
+// let a = "10"
+// let b = 10
+// console.log(a+b)
+// console.log(a*b)
+
+// let a = 2.01
+// let b = 2.01
+// console.log(a==b)
+
+// const a = "abc"           //    we cannot reassign
+// a = "za"
+// console.log(a)
+
+var name = "ritik"
+console.log(name);
