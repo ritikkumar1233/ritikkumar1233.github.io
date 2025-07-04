36a5cc61d357f665f436e4180d2e14eec0afeb77
ritikkumar1233 <ritikroushan96905@gmail.com>
2025-07-04 16:20:34 +0530

    ok

--- nodejs/nodeapp/index9.js
+++ nodejs/nodeapp/index9.js
@@ -26,16 +26,38 @@ const users = [
 ];
 
 app.use(express.json());
+
+const authenticate = (req, res, next)=>{
+    try{
+        let token = req.headers.authorization;
+        token = token.split(" ")[1];
+        const user = jwt.verify(token, SECRET);
+        req.role = user.role
+        next();
+    }
+    catch(err){
+        return req.json({message: "Access Denied"});
+    }
+
+    // res.json({message: "Access Denied"});
+    // next();
+};
+
 app.post("/login", (req, res)=>{
     const {email, password} = req.body;
     const found = users.find(
         (user)=>user.email === email && user.password === password
     );
     if(found){
-        const token = jwt.sign(found, SECRET)
+        const token = jwt.sign(found, SECRET, {expiresIn: "1h"});
         res.status(200).json({user: found, token});
     }
     else{
         res.status(400).json({message: "Access Denied"})
     }
+})
+
+
+app.get("/users", authenticate ,(req, res)=>{
+    res.json(users);
 })
